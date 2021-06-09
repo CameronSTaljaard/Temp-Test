e25be00de58d1c530b274c215223f4c14149c91c
CameronSTaljaard <46622484+CameronSTaljaard@users.noreply.github.com>
2021-06-09 15:04:23 +0200

    Test

--- src/app/plugin.ts
+++ src/app/plugin.ts
@@ -6,7 +6,7 @@ export class DemoPlugin {
             const name = data['ctx.userName'];
             const email = data['ctx.userEmail'];
             console.log(data['ctx.userName']);
-            console.log(data['ctx.userEmail']);
+            console.log(data['context.userEmail']);
             this.changeVariables(name, email);
         });
     }
