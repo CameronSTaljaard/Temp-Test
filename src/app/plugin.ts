7c0d2d8ae50abad8aee0ccb2dbd4349067711ffa
CameronSTaljaard <46622484+CameronSTaljaard@users.noreply.github.com>
2021-06-09 13:05:17 +0200

    Email test.

--- src/app/plugin.ts
+++ src/app/plugin.ts
@@ -4,12 +4,19 @@ export class DemoPlugin {
     constructor() {
         new PluginAdapter().init().then(data => {
             const name = data['ctx.userName'];
+            const email = data['ctx.userEmail'];
             this.changeName(name);
+            this.changeEmail(email);
         });
     }
 
     private changeName(userName: string) {
         const nameElem = document.getElementById('userName')!;
         nameElem.innerText = userName;
+    }
+
+    private changeEmail(userEmail: string) {
+        const emailElem = document.getElementById('userEmail')!;
+        emailElem.innerText = userEmail;
     }
 }
