0c89cd8c80636ae7ba5b80fe18c80fd588bedd95
CameronSTaljaard <46622484+CameronSTaljaard@users.noreply.github.com>
2021-06-09 14:44:04 +0200

    Test

--- src/app/plugin.ts
+++ src/app/plugin.ts
@@ -5,18 +5,24 @@ export class DemoPlugin {
         new PluginAdapter().init().then(data => {
             const name = data['ctx.userName'];
             const email = data['ctx.userEmail'];
-            this.changeName(name);
-            this.changeEmail(email);
+            this.changeVariables(name, email);
         });
     }
 
-    private changeName(userName: string) {
+    private changeVariables(userName: string, userEmail: string) {
         const nameElem = document.getElementById('userName')!;
         nameElem.innerText = userName;
-    }
-
-    private changeEmail(userEmail: string) {
         const emailElem = document.getElementById('userEmail')!;
         emailElem.innerText = userEmail;
     }
+
+    //private changeName(userName: string) {
+    //    const nameElem = document.getElementById('userName')!;
+    //    nameElem.innerText = userName;
+    //}
+
+    //private changeEmail(userEmail: string) {
+    //    const emailElem = document.getElementById('userEmail')!;
+    //    emailElem.innerText = userEmail;
+    //}
 }
