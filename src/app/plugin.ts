cf6dc4109d6360a437338e8f6adfc879a5fdf4f6
CameronSTaljaard <46622484+CameronSTaljaard@users.noreply.github.com>
2021-06-09 14:07:52 +0200

    Email test.

--- src/app/plugin.ts
+++ src/app/plugin.ts
@@ -1,11 +1,10 @@
 import {PluginAdapter} from '@coyoapp/plugin-adapter';
-import jwtDecode from 'jwt-decode';
 
 export class DemoPlugin {
     constructor() {
         new PluginAdapter().init().then(data => {
             const name = data['ctx.userName'];
-            const email = jwtDecode["data.userEmail"];
+            const email = data['ctx.userEmail'];
             this.changeName(name);
             this.changeEmail(email);
         });
