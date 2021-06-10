3ea92b365465da3754e90b0e10f1c0dd9f1e1d86
CameronSTaljaard <46622484+CameronSTaljaard@users.noreply.github.com>
2021-06-10 12:49:14 +0200

    Changing default routes to html, removed view engine pug.

--- widget.js
+++ widget.js
@@ -1,8 +1,6 @@
 const express = require('express')
 const app = express()
-var router = express.Router();
 const PORT = process.env.PORT || 5000
-const manifestFile = require("./public/manifest.json");
 
 // App engine
 app.use(express.static(__dirname + '/public'));
@@ -12,24 +10,9 @@ app.use(express.static('./src/scripts'));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-// App routes
-const landingPage = require('./api/routes/index');
-
 // Route handlers
 //router.get('/', function (req, res) {
 //  res.render('index');
 //});
-
-router.get('/', (req, res, next) => {
-	res.render('index', {
-		title:'Landing Page',
-	});
-});
-
-router.get('/manifest.json', (req, res, next) => {
-	res.json(manifestFile);
-});
-
-app.use('/', router);
 
 app.listen(PORT,() => console.log(`Server is running on ${ PORT }`));
