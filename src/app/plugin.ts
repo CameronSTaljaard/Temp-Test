9efe4f2924cf75827ba41433d11ff52de7ba9398
CameronSTaljaard <46622484+CameronSTaljaard@users.noreply.github.com>
2021-06-10 13:35:12 +0200

    Converted plugin back to typescript

--- src/app/plugin.ts
+++ src/app/plugin.ts
@@ -6,7 +6,7 @@ export class DemoPlugin {
             const name = data['ctx.userName'];
             const email = data['ctx.userEmail'];
             this.updateVariables(name, email);
-            //this.accountLookup(email);
+            this.accountLookup(email);
         });
     }
 
@@ -16,4 +16,22 @@ export class DemoPlugin {
         const emailElem = document.getElementById('userEmail');
         emailElem.innerText = userEmail;
     }
+
+    accountLookup(email) {
+      let url = "cameronstaljaard@gmail.com";
+      let lookup = "MF7FXPqcrBQENtmQoUnE";
+      (async () => {
+        const rawResponse = await fetch(url, {
+          method: 'POST',
+          headers: {
+            'email': email,
+            'lookup_token': lookup,
+            'Content-Type': 'application/json'
+          },
+          body: JSON.stringify({a: 1, b: 'Textual content'})
+        });
+        const content = await rawResponse.json();
+      
+        console.log(content);
+      })();
 }
