11b3b269abf7c1110863f62f83a1d8b24d42cb85
CameronSTaljaard <46622484+CameronSTaljaard@users.noreply.github.com>
2021-06-10 12:56:59 +0200

    Fixed missing index content.

--- widget.js
+++ widget.js
@@ -1,18 +1,9 @@
 const express = require('express')
 const app = express()
+const path = require('path')
 const PORT = process.env.PORT || 5000
 
 // App engine
 app.use(express.static(__dirname + '/public'));
-app.use(express.static('./src/styles'));
-app.use(express.static('./src/images'));
-app.use(express.static('./src/scripts'));
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
-
-// Route handlers
-//router.get('/', function (req, res) {
-//  res.render('index');
-//});
 
 app.listen(PORT,() => console.log(`Server is running on ${ PORT }`));
